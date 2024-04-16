8bfb6cd6862a1a8f7618c5402df68eab5b110994
Baptiste <lantranbaptiste@gmail.com>
2024-04-16 17:46:19 +0200

    Added more projects and a slider

---
+++ js/slider.js
@@ -0,0 +1,31 @@
+const slide = [
+    "accueil.png",
+    "accueil2.png",
+    "accueil3.png",
+    "coinflip.PNG",
+    "Graphiques.png",
+    "HiLo.PNG",
+    "humour.PNG",
+    "humour2.PNG",
+    "jobistic.PNG",
+    "Share-my-craft.PNG",
+    "simon.PNG",
+    "tindogs.PNG",
+    "tir.png",
+    "tout.png"
+];
+let numero = 0;
+
+
+
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slider-img").src = 'images/slider_project_images/' + slide[numero];
+}
+
+setInterval("ChangeSlide(1)", 4000);
